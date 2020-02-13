97eee8f87937160edf8032d619fd1f239d0924e6
DuanXueZhi <38554820+DuanXueZhi@users.noreply.github.com>
2020-02-13 18:29:59 +0800

    7-3项目配置注释

--- vue-element-admin-master/src/settings.js
+++ vue-element-admin-master/src/settings.js
@@ -1,29 +1,29 @@
 module.exports = {
-  title: 'Vue Element Admin',
+  title: 'Vue Element Admin项目标题', // 项目标题
 
   /**
    * @type {boolean} true | false
    * @description Whether show the settings right-panel
    */
-  showSettings: true,
+  showSettings: true, // 右侧控制面板是否显示
 
   /**
    * @type {boolean} true | false
    * @description Whether need tagsView
    */
-  tagsView: true,
+  tagsView: true, // 页面顶部打开标签导航
 
   /**
    * @type {boolean} true | false
    * @description Whether fix the header
    */
-  fixedHeader: false,
+  fixedHeader: false, // 整体页面顶部是否fixed
 
   /**
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
    */
-  sidebarLogo: false,
+  sidebarLogo: false, // 左侧菜单栏顶部是否展示企业logo
 
   /**
    * @type {string | array} 'production' | ['production', 'development']
@@ -31,5 +31,5 @@ module.exports = {
    * The default is only used in the production env
    * If you want to also use it in dev, you can pass ['production', 'development']
    */
-  errorLog: 'production'
+  errorLog: 'production' // 页面错误日志
 }
